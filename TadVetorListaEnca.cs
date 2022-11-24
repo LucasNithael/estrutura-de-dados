5022cceefe87a81327ad8620c501f60a361282e2
Lucas Nithael <94084548+LucasNithael@users.noreply.github.com>
2022-11-24 01:03:13 +0000

    Tad Vetor com Lista duplamente encadeada

--- TadVetorListaEnca.cs
+++ TadVetorListaEnca.cs
@@ -3,165 +3,146 @@ using System;
 class MainClass{
   public static void Main(){
     Vector v = new Vector();
-    v.Inserir_Inicio(111);
-    v.Inserir_Inicio(100);
-    v.Inserir_Inicio(5);
-    v.Inserir_Inicio(6);
-    Console.WriteLine("Size:"+v.size);
-    Node x = (Node)v.elementAtRank(1);
-    v.insertAtRank(4, x);
-    //v.removeAtRank(1);
-    //v.Remove_Fim();
-    v.Ver_Lista();
+    v.insertAtRank(0, 1);
+    v.insertAtRank(1, 2);
+    v.insertAtRank(2, 3);
+    v.insertAtRank(3, 4);
+    v.replaceAtRank(3, "P");
+    v.removeAtRank(1);
+    Console.WriteLine("ElementAtRank:"+v.elementAtRank(3));
+    v.Ver_Lista_i();
+    
+    v.Ver_Lista_f();
     
   }
 }
 
+
+/************ Classe Do Vetor **************/
 class Vector{
-  private Node first, last;
-  public int size;
-
-  public void Inserir_Inicio(object o){
-    Node new_ = new Node(o);
-    if(size==0){
-      first = new_;
-      last = new_;
-    }
-    else{
-      first.SetPrev(new_);
-      new_.SetNext(first);
-      first = new_;
-    }
-   size++;   
+  private Node i,f;
+  private int size;
+  
+  public Vector(){
+    i = new Node(null);
+    f = new Node(null);
+    i.SetNext(f);
+    f.SetPrev(i);
   }
-
-  public object Remove_Fim(){
-    if(size==0)
-      throw new VectorVazia("Vetor Vazio");
-    object temp = last;
-    Node anterior_last = last.GetPrev();
-    anterior_last.SetNext(null);
-    last = anterior_last;
-    size--;
-    return temp;
-  }
-
   public object elementAtRank(int r){
     if(size==0)
       throw new VectorVazia("Vetor Vazio");
     if(r>=size)
       throw new IndiceForadeFaixa("Índice fora de faixa");
-    Node current = first;
+    Node current = i;
     while(r!=0){
       current = current.GetNext(); 
       r--;
     }
-    return current;
+    return current.GetNext().GetElement();
   }
 
   public object replaceAtRank(int r, object o){
-    if(size==0)
-      throw new VectorVazia("Vetor Vazio");
-    if(r>size)
-      throw new IndiceForadeFaixa("Índice fora de faixa");
-    if(r==size){
-      Node node = (Node)o;
-      Node novo = new Node(node.GetElement());
-      novo.SetNext(null);
-      novo.SetPrev(last);
-      last.SetNext(novo);
-      last = novo;
-      size++;
-      return null;
-    }
-    else{
-      Node current = first;
-      while(r!=0){
-        current = current.GetNext();
-        r--;
-      }
-      Node temp = current;
-      Node aux = (Node)o;
-      current.SetElement(aux.GetElement());
-    return temp;
-    }
-  }
-
-  public void insertAtRank(int r, object o){
-    if(size==0)
-      throw new VectorVazia("Vetor Vazio");
-    if(r>size)
-      throw new IndiceForadeFaixa("Índice fora de faixa");
-    if(r==size){
-      Node node = (Node)o;
-      Node novo = new Node(node.GetElement());
-      novo.SetNext(null);
-      novo.SetPrev(last);
-      last.SetNext(novo);
-      last = novo;
-      size++;
-    }
-    else{
-      Node current = first;
-      while(r!=0){
-        current = current.GetNext();
-        r--;
-      }
-      Node temp = current;
-      Node aux = (Node)o;
-      current.SetElement(aux.GetElement());
-    }
-  }
-
-  public object removeAtRank(int r){
-    if(size==0)
+    if(isEmpty())
       throw new VectorVazia("Vetor Vazio");
     if(r>=size)
       throw new IndiceForadeFaixa("Índice fora de faixa");
-    Node b = first;
-    int indexAux = r;
+    Node current = i;
     while(r!=0){
-      b = b.GetNext();
+      current = current.GetNext();
       r--;
     }
-    Node temp = b;
-    Console.WriteLine(temp.GetElement());
-    // [A] <-> [B] <-> [c]
-    if(b==last){
-      Node before_last = b.GetPrev();
-      before_last.SetNext(null);
-      last = before_last;
+    object temp = current.GetNext().GetElement(); 
+    Node current_next = current.GetNext();
+    current_next.SetElement(o);
+    return temp;
+  }
+
+  public void insertAtRank(int r, object o){
+    if(r>size)
+      throw new IndiceForadeFaixa("Índice fora de faixa");
+    Node new_ = new Node(o);
+    if(r==0 && isEmpty()){
+      i.SetNext(new_);
+      f.SetPrev(new_);
     }
-    else if(indexAux==0)
-      first = b.GetNext();
+    else if(r==size){
+      Node previus_f = f.GetPrev();
+      new_.SetNext(f);
+      new_.SetPrev(f.GetPrev());
+      previus_f.SetNext(new_);
+      f.SetPrev(new_);
+    }
     else{
-      Node a = b.GetPrev();
-      Node c = b.GetNext();
-      a.SetNext(c);
-      c.SetPrev(a);
+      Node current = i;
+      while(r!=0){
+        current = current.GetNext();
+        r--;
+      }
+      if(current==i){
+        Node previus_next_i = i.GetNext();
+        new_.SetNext(i.GetNext());
+        new_.SetPrev(i);
+        previus_next_i.SetPrev(new_);
+        i.SetNext(new_);
+      }
+      else{
+        new_.SetPrev(current);
+        new_.SetNext(current.GetNext());
+        Node current_next = current.GetNext();
+        current_next.SetPrev(new_);
+        current.SetNext(new_);
+      }
     }
+    size++;
+  }
+
+  public object removeAtRank(int r){
+    if(isEmpty())
+      throw new VectorVazia("Vetor Vazio");
+    if(r>=size)
+      throw new IndiceForadeFaixa("Índice fora de faixa");
+    Node current = i;
+    while(r!=0){
+      current = current.GetNext();
+      r--;
+    }
+    
+    object temp = current.GetNext().GetElement();
+    Node current_next = current.GetNext();
+    Node current_next_next = current_next.GetNext();
+    current.SetNext(current_next_next);
+    current_next_next.SetPrev(current);
     size--;
     return temp;
-    
   }
 
   public int Size(){
     return size;
   }
-
+  
   public Boolean isEmpty(){
-    return size!=0;
+    return size==0;
   }
   
-  public void Ver_Lista(){
-    Node current = first;
+  public void Ver_Lista_i(){
+    Node current = i;
     while(current!=null){
       Console.Write(current.GetElement()+"<->");
       current = current.GetNext();
     }
   }
+  
+  public void Ver_Lista_f(){
+   Node current = f;
+   while(current!=null){
+     Console.Write(current.GetElement()+"<->");
+     current = current.GetPrev();
+   }
+  }
 }
 
+/************ Classe Do Nó **************/
 class Node{
   private object element;
   private Node next;
